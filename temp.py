2bee58a87256c697e04f645abbb507db2621f954
wangjinyu <wangjinyu124419@gmail.com>
2019-12-25 03:29:46 +0800

    1225

---
+++ temp.py
@@ -0,0 +1,14 @@
+import twint
+import os
+# Configure
+# c = twint.Config()
+# c.Username = "Ding1204"
+# # c.Media = True
+# c.Replies =False
+# c.Retweets =False
+# c.Output = 'Ding1204.txt'
+# twint.run.Search(c)
+
+abs_path = os.path.abspath(__file__)
+fahter_path = os.path.dirname(abs_path)
+a=1
