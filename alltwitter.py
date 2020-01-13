4cb5a6b71f5537c1322d6a5b4e659676746bab75
wangjinyu <wangjinyu124419@gmail.com>
2020-01-14 00:01:16 +0800

    add download one user

--- alltwitter.py
+++ alltwitter.py
@@ -3,20 +3,21 @@ import sys
 import locale
 import time
 import traceback
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor,as_completed
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
 from retry import retry
 
-
 from twint import twint
+
+
 # from twitter import count_time
 
 
 # Configure
 class AllTwitter():
-    def __init__(self, user=None):
+    def __init__(self, user=''):
         self.user = user
-        self.following_file = user+'.txt'
-        self.save_dir='miss'
+        self.following_file = self.user + '.txt'
+        self.save_dir = 'follow'
 
     def get_all_followling(self):
         c = twint.Config()
@@ -29,15 +30,16 @@ class AllTwitter():
         # self.following_file=c.Output
         twint.run.Following(c)
         self.resort()
+
     def resort(self):
         with open(self.following_file, encoding='utf8') as f:
-            following_list=f.readlines()
+            following_list = f.readlines()
             following_list.sort()
         with open(self.following_file, 'w', encoding='utf8') as f:
             f.writelines(following_list)
 
-    @retry(delay=10,tries=5, backoff=2, max_delay=160)
-    def get_one_user(self,follow):
+    # @retry(delay=10, tries=5, backoff=2, max_delay=160)
+    def get_one_user(self, follow):
         c = twint.Config()
         c.Proxy_host = '127.0.0.1'
         c.Proxy_port = '1080'
@@ -47,31 +49,26 @@ class AllTwitter():
         c.Output = os.path.join(self.save_dir, follow + '.txt')
         # c.Media = True
         # c.Videos = True
-        # c.Since = '2019-12-01'
+        # c.Since = '2019-12-20'
+        # c.Until = '2020-01-08'
         twint.run.Search(c)
         # twint.run.Profile(c)
-        # twint.run.Search(c)
-        # twint.run.Profile(c)
-        print('%s已完成'%follow)
+        print('%s已完成' % follow)
         with open('finish.txt', 'a') as f:
-            f.write(follow+'\n')
+            f.write(follow + '\n')
+
     # @count_time
     def get_all_twitter(self):
         with open(self.following_file, encoding='utf8') as f:
             follow_list = [follow.strip() for follow in f.readlines()]
-            t_list = []
-
-            with ProcessPoolExecutor() as executor:
-                t_dict={executor.submit(self.get_one_user, follow):follow for follow in follow_list}
+            with ProcessPoolExecutor(max_workers=1) as executor:
+                t_dict = {executor.submit(self.get_one_user, follow): follow for follow in follow_list}
                 for future in as_completed(t_dict):
-                    follow=t_dict[future]
+                    follow = t_dict[future]
                     try:
                         future.result()
                     except Exception:
-                        print('get_one_user异常:%s'%follow+'\n',traceback.format_exc())
-
-
-
+                        print('get_one_user异常:%s' % follow + '\n', traceback.format_exc())
 
     def get_pic_url(self):
         pass
@@ -83,6 +80,7 @@ class AllTwitter():
 
 if __name__ == '__main__':
     at = AllTwitter('miss')
-    at.get_all_twitter()
+    # at.get_all_twitter()
     # at.get_all_followling()
+    at.get_one_user('dongxiaoniu')
     # at.main()
--- twitter.py
+++ twitter.py
@@ -17,15 +17,18 @@ from alltwitter import AllTwitter
 
 lock = multiprocessing.Lock()
 base_dir = r'K:\爬虫\twitter\download_twitter'
-pre_url='https://twitter.com/'
+pre_url = 'https://twitter.com/'
+
+
 def count_time(fun):
     def warpper(*args):
         s_time = time.time()
-        arg=args[1] if len(args)>1 else '-'
+        arg = args[1] if len(args) > 1 else '-'
         fun(*args)
         e_time = time.time()
         t_time = e_time - s_time
-        print('%s耗时：%s,参数:%s'% (fun.__name__, t_time,arg))
+        print('%s耗时：%s,参数:%s' % (fun.__name__, t_time, arg))
+
     return warpper
 
 
@@ -35,26 +38,26 @@ class Twitter():
             'http': 'http://127.0.0.1:1080',
             'https': 'https://127.0.0.1:1080',
         }
+        # self.follow_dir = 'memberlist'
         self.follow_dir = 'follow'
-        self.recorder_file='recorder_file.txt'
+        self.recorder_file = 'recorder_file.txt'
         with open(self.recorder_file) as f:
-            self.recoder_list = [ url.strip() for url in f.readlines()]
+            self.recoder_list = [url.strip() for url in f.readlines()]
         if not os.path.exists(self.recorder_file):
-            with open(self.recorder_file,'w') as f:
+            with open(self.recorder_file, 'w') as f:
                 pass
         # self.w_file=open(self.recorder_file,'w')
         # self.r_file=open(self.recorder_file,'r')
 
-    @retry(delay=10,tries=5, backoff=2, max_delay=160)
-    def get_nickname(self,follow):
-        url=pre_url+follow
+    # @retry(delay=10, tries=5, backoff=2, max_delay=160)
+    def get_nickname(self, follow):
+        url = pre_url + follow
         try:
             page_source = requests.get(url, timeout=30, proxies=self.proxies).text
             selector = etree.HTML(page_source)
             nickname = selector.xpath('//h1[@class="ProfileHeaderCard-name"]/a/text()')[0]
-
         except:
-            nickname=follow
+            nickname = follow
         legal_nickname = re.sub(r"[\/\\\:\*\?\"\<\>\|!！\.\s]", "", nickname)
         return legal_nickname
 
@@ -81,10 +84,9 @@ class Twitter():
         # if not pic_urls:
         pic_urls = selector.xpath('//meta[starts-with(@content,"https://pbs.twimg.com/media")]/@content')
 
-
         gif_urls = selector.xpath('//meta[starts-with(@content,"https://pbs.twimg.com/tweet_video_thumb")]/@content')
 
-            # print('url:%s\npic_urls:%s'%(url,pic_urls))
+        # print('url:%s\npic_urls:%s'%(url,pic_urls))
         return pic_urls, gif_urls
 
     # @retry(delay=10, backoff=2, max_delay=160)
@@ -119,7 +121,7 @@ class Twitter():
         l = os.listdir(path)
         print('下载数量:%d' % len(l))
 
-    def check_repeat(self,url):
+    def check_repeat(self, url):
         try:
             if url in self.recoder_list:
                 print('已经下载:%s' % url)
@@ -132,22 +134,23 @@ class Twitter():
         except Exception:
             print(traceback.format_exc())
 
-    def record_repeat(self,url):
-        with open(self.recorder_file,'a') as f:
+    def record_repeat(self, url):
+        with open(self.recorder_file, 'a') as f:
             f.write(url + '\n')
+
     # @count_time
-    @retry(delay=10,tries=5, backoff=2, max_delay=160)
+    # @retry(delay=10, tries=5, backoff=2, max_delay=160)
     def download(self, pic_t, path):
-        status_code = requests.get(pic_t, proxies=self.proxies,timeout=30).status_code
-        if status_code!=200:
-            print('%s状态码错误:%d'%(pic_t,status_code))
+        status_code = requests.get(pic_t, proxies=self.proxies, timeout=30).status_code
+        if status_code != 200:
+            print('%s状态码错误:%d' % (pic_t, status_code))
             self.record_repeat(pic_t)
             return
-        real_url = requests.get(pic_t, proxies=self.proxies,timeout=30).url
-        if 'status'  not in real_url:
-            print("地址异常:%s"%real_url)
+        real_url = requests.get(pic_t, proxies=self.proxies, timeout=30).url
+        if 'status' not in real_url:
+            print("地址异常:%s" % real_url)
             return
-        print("当前执行的线程为:%s,url:%s"%(threading.current_thread(),real_url))
+        print("当前执行的线程为:%s,url:%s" % (threading.current_thread(), real_url))
         pic_urls, gif_urls = self.get_pics_url(real_url)
         if pic_urls:
             for pic_url in pic_urls:
@@ -157,47 +160,31 @@ class Twitter():
                 self.downloadgif(pic_url, path)
         if not pic_urls and not gif_urls:
             try:
-                twitter_download(real_url,output_dir=path)
+                twitter_download(real_url, output_dir=path)
             except Exception as e:
-                print('twitter_download异常:%s'%pic_t,e)
+                print('twitter_download异常:%s' % pic_t, e)
                 try:
                     twitter_dl = TwitterDownloader(real_url, output_dir=path)
                     lock.acquire()
-                    print("上锁:%s"%real_url)
+                    print("上锁:%s" % real_url)
                     twitter_dl.download()
                 finally:
                     lock.release()
                     print("解锁:%s" % real_url)
         self.record_repeat(pic_t)
 
+    def thread_download(self,path,pic_t_list):
+        # thead pool
+        with ThreadPoolExecutor() as executor:
+            t_dict = {executor.submit(self.download, pic_t, path): pic_t for pic_t in pic_t_list if
+                      not self.check_repeat(pic_t)}
+            for future in as_completed(t_dict):
+                url = t_dict[future]
+                try:
+                    future.result()
+                except Exception:
+                    print('download异常:%s' % url + '\n', traceback.format_exc())
 
-
-    @count_time
-    def main(self):
-        # at = AllTwitter(user='jordan124419')
-        # at.main()
-        # at.get_all_twitter()
-        follow_list = os.listdir(self.follow_dir)
-        for follow in follow_list:
-            print('download user:%s' % follow)
-            nick_name = self.get_nickname(follow.split('.')[0])
-            path = os.path.join(base_dir,nick_name)
-            if not os.path.exists(path):
-                os.makedirs(path)
-                print('创建：%s'%nick_name)
-            pic_t_list = self.get_twitter(os.path.join(self.follow_dir, follow))
-            # pic_t_list = ['https://pic.twitter.com/LYvQA7I5Dn','123']
-            print('pic_t_list:%s' % len(pic_t_list))
-
-            # thead pool
-            with ThreadPoolExecutor() as executor:
-                t_dict={executor.submit(self.download, pic_t, path):pic_t for pic_t in pic_t_list if not self.check_repeat(pic_t) }
-                for future in as_completed(t_dict):
-                    url=t_dict[future]
-                    try:
-                        future.result()
-                    except Exception:
-                        print('download异常:%s'%url+'\n',traceback.format_exc())
             # process pool
             # p_list = []
             # with ProcessPoolExecutor() as executor:
@@ -214,9 +201,40 @@ class Twitter():
             # single thread
             # for pic_t in pic_t_list:
             #     self.download(pic_t,path)
+
+    def download_one(self,user=None):
+        at = AllTwitter()
+        at.get_one_user(user)
+        nick_name = self.get_nickname(user)
+        path = os.path.join(base_dir, nick_name)
+        if not os.path.exists(path):
+            os.makedirs(path)
+            print('创建：%s' % nick_name)
+        pic_t_list = self.get_twitter(os.path.join(self.follow_dir, user+'.txt'))
+        self.thread_download(path,pic_t_list)
+        self.count_pic(path)
+    @count_time
+    def main(self):
+        at = AllTwitter(user='memberlist')
+        # at.main()
+        # at.get_all_twitter()
+        at.get_one_user('feitunmugou091')
+        follow_list = os.listdir(self.follow_dir)
+        for follow in follow_list:
+            print('download user:%s' % follow)
+            nick_name = self.get_nickname(follow.split('.')[0])
+            path = os.path.join(base_dir, nick_name)
+            if not os.path.exists(path):
+                os.makedirs(path)
+                print('创建：%s' % nick_name)
+            pic_t_list = self.get_twitter(os.path.join(self.follow_dir, follow))
+            # pic_t_list = ['https://pic.twitter.com/LYvQA7I5Dn','123']
+            print('pic_t_list:%s' % len(pic_t_list))
+            self.thread_download(path,pic_t_list)
             self.count_pic(path)
 
 
 if __name__ == '__main__':
     t = Twitter()
-    t.main()
+    t.download_one('yaojing2019')
+    # t.main()
